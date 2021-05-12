fb2b57e0070b5c01a9ec76650eaa9ae2df5302cc
tanya.pog <tanya.pog@emms.ru>
2021-05-12 17:21:17 +0300

    model of Category and moking CategoryRepository

---
+++ lib/category_repository.dart
@@ -0,0 +1,19 @@
+import 'package:different_draggable_lists/model/category.dart';
+
+class CategoryRepository {
+  final List<Category> categories = [
+    Category(name: 'Снаряжение', position: 1),
+    Category(name: 'Посуда', position: 2),
+    Category(name: 'Одежда ходовая', position: 3),
+    Category(name: 'Одежда для бивака', position: 4),
+    Category(name: 'Одежда приличная', position: 5),
+    Category(name: 'Обувь', position: 6),
+    Category(name: 'Спортивное', position: 7),
+    Category(name: 'Девайсы', position: 8),
+    Category(name: 'Документы', position: 9),
+    Category(name: 'Аптечка', position: 10),
+    Category(name: 'Косметичка', position: 11),
+    Category(name: 'Другое', position: 12),
+    Category(name: 'Еда', position: 13),
+  ];
+}
---
+++ lib/model/category.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class Category {
+  String name;
+  int position;
+
+  Category({
+    @required this.name,
+    @required this.position
+  });
+
+  factory Category.empty() => Category(name: '', position: null);
+
+  @override
+  String toString() {
+    return "${this.position}-${this.name}";
+  }
+}
